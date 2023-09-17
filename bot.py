38e25347705db7bb5c9acc4685273e3533205224
slvjzz <salvo241@gmail.com>
2023-09-18 02:02:29 +0300

    test2

--- bot.py
+++ bot.py
@@ -1,20 +1,40 @@
 import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 import bot_api
 
 BOT_API_KEY = '6569744404:AAEGTiIWt0L2486WsISA1j6WSoSJJP83k0o'
 
+#TEST
 bot = telebot.TeleBot(BOT_API_KEY)
 
 
 @bot.message_handler(commands=['start'])
 def bot_home(message):
-    responce = ''
-    req = bot_api.get_home()
-    for key in req.keys():
-        responce += f'{key}:     {req.get(key)}\n'
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = InlineKeyboardButton(text='/Лоты',)
+    markup.add(btn1)
+
+    resp = f"""Привет. Это бот гаражной распродажи. """
 
     bot.send_message(message.chat.id,
-                     text=responce)
+                     text=resp, reply_markup=markup)
+
+
+@bot.message_handler(commands=['Лоты'])
+def lots(message):
+    responce = ''
+    req = bot_api.get_home()
+
+    keyboard = ReplyKeyboardMarkup()
+
+    for key in req.keys():
+        button = InlineKeyboardButton(text=req.get(key).get("name"))
+        keyboard.add(button)
+
+        responce += f'{req.get(key).get("name")}: {req.get(key).get("sale_price")} {req.get(key).get("currency")}\n'
+
+    bot.send_message(message.chat.id,
+                     text=responce, reply_markup=keyboard)
 
 
 bot.polling(none_stop=True, interval=0)
--- bot_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-#TEST
-BASE_URL = 'http://127.0.0.1:5000/bot'
-
-
-def get_home():
-    home = requests.get(BASE_URL+'/lots')
-    return home.json()
