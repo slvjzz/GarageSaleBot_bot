8582836ead20e16d3e6e1533638be7dd7e427c37
slvjzz <salvo241@gmail.com>
2023-09-19 10:44:00 +0300

    Bot Started work

--- bot.py
+++ bot.py
@@ -1,39 +1,93 @@
-import telebot
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 import bot_api
 
 BOT_API_KEY = '6569744404:AAEGTiIWt0L2486WsISA1j6WSoSJJP83k0o'
 
-bot = telebot.TeleBot(BOT_API_KEY)
+bot = AsyncTeleBot(BOT_API_KEY)
 
 
 @bot.message_handler(commands=['start'])
-def bot_home(message):
+async def bot_home(message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = InlineKeyboardButton(text='/Лоты',)
-    markup.add(btn1)
+    lots_button = InlineKeyboardButton(text='Лоты', )
+    categories = InlineKeyboardButton(text='Категории', )
+    markup.add(lots_button)
+    markup.add(categories)
 
     resp = f"""Привет. Это бот гаражной распродажи. """
 
-    bot.send_message(message.chat.id,
-                     text=resp, reply_markup=markup)
+    await bot.send_message(message.chat.id,
+                           text=resp, reply_markup=markup)
 
 
-@bot.message_handler(commands=['Лоты'])
-def lots(message):
-    responce = ''
-    req = bot_api.get_home()
+@bot.message_handler(func=lambda message: True)
+async def handle_callback_query(message):
+    if message.text == 'Лоты':
+        responce = ''
+        req = bot_api.get_lots()
 
-    keyboard = ReplyKeyboardMarkup()
+        keyboard = InlineKeyboardMarkup()
 
-    for key in req.keys():
-        button = KeyboardButton(text=req.get(key).get("name"))
-        keyboard.add(button)
+        for item in req:
+            lot_id = item.get("id")
+            button_text = item.get("name")
+            button = InlineKeyboardButton(text=button_text, callback_data=f'lot_{lot_id}')
+            keyboard.add(button)
 
-        responce += f'{req.get(key).get("name")}: {req.get(key).get("sale_price")} {req.get(key).get("currency")}\n'
+            responce += f'{lot_id}. {item.get("name")}: {item.get("sale_price")} {item.get("currency")}\n'
+        await bot.send_message(message.chat.id, text=responce, reply_markup=keyboard)
 
-    bot.send_message(message.chat.id,
-                     text=responce, reply_markup=keyboard)
+    elif message.text == 'Категории':
+        responce = ''
+        req = bot_api.get_categories()
+
+        keyboard = ReplyKeyboardMarkup()
+
+        for item in req:
+            button = KeyboardButton(text=item.get("name"))
+            keyboard.add(button)
+
+            responce += f'{item.get("name")}: {item.get("sale_price")} {item.get("currency")}\n'
+
+        menu = InlineKeyboardButton(text='Меню', )
+        keyboard.add(menu)
+        await bot.send_message(message.chat.id, text=responce, reply_markup=keyboard)
+
+    elif message.text == 'Меню':
+
+        markup = ReplyKeyboardMarkup(resize_keyboard=True)
+        lots_button = InlineKeyboardButton(text='Лоты', )
+        categories = InlineKeyboardButton(text='Категории', )
+        markup.add(lots_button)
+        markup.add(categories)
+
+        await bot.send_message(message.chat.id, text='Главное меню', reply_markup=markup)
+
+    elif message.text.startswith('Лот_'):
+        id = message.text.split('_')[1].split(':')[0]
+        req = bot_api.get_lot(id)
+        print(req)
 
 
-bot.polling(none_stop=True, interval=0)
+@bot.callback_query_handler(func=lambda call: True)
+async def handle_callback_query(call):
+    print('CALLL!!!!!')
+    print(call)
+    if call.data.startswith('lot_'):
+        id = call.data.split('_')[1]
+        req = bot_api.get_lot(id)
+        message = f"{req.get('name')}\nОписание:{req.get('description')}\nЦена: {req.get('sale_price')} {req.get('currency')}"
+        await bot.send_message(call.message.chat.id, text=message)
+    #    lot_id = int(call.data.split('_')[1])
+    #     response = requests.get(f'http://your_flask_server/bot/lots/{lot_id}')
+    #     if response.status_code == 200:
+    #         lot_data = response.json()
+    #         # Обработайте данные лота, например, отправьте их обратно пользователю в чате бота.
+    # elif call.data == 'menu':
+    #     # Обработка нажатия на кнопку "Меню"
+    #     # Добавьте ваш код для обработки этой кнопки
+    #     pass
+
+asyncio.run(bot.polling(none_stop=True, interval=0))
--- bot_api.py
+++ bot_api.py
@@ -3,6 +3,17 @@ import requests
 BASE_URL = 'http://127.0.0.1:5000/bot'
 
 
-def get_home():
+def get_lots():
     home = requests.get(BASE_URL+'/lots')
     return home.json()
+
+
+def get_categories():
+    print('GET')
+    categories = requests.get(BASE_URL+'/categories')
+    return categories.json()
+
+
+def get_lot(id):
+    lot = requests.get(BASE_URL+f'/lots/{id}')
+    return lot.json()
