1810a244cda2ca7733feeb04a3a1af816e5daaf0
slvjzz <salvo241@gmail.com>
2023-09-27 00:28:42 +0300

    Some Fixes

--- bot.py
+++ bot.py
@@ -26,30 +26,38 @@ async def get_photo(id):
 
 
 async def get_lot(id, chat_id):
-    auction_id=''
+    auction_id = ''
     media = await get_photo(id)
     req = bot_api.get_lot(id)
+    if req.get('max_bid_amount') is None:
+        bid = round(float(req.get('auction_start_price')), 2)
+    else:
+        bid = round(float(req.get('max_bid_amount')), 2)
+    if bid == 0:
+        bid += 1
+    new_bid = round(float(bid*1.05), 2)
 
     message = f"{req.get('name')}\nОписание: {req.get('description')} \nЦена: {req.get('sale_price')} {req.get('currency')}"
     keyboard = InlineKeyboardMarkup(row_width=3)
 
+    button_buy = InlineKeyboardButton(text='Купить', callback_data=f"buy_{id}")
+    keyboard.add(button_buy)
+
     if req['auction_id'] is not None:
         auction_id = req['auction_id']
         message = message.replace('\nЦена', '\nКупить сразу')
-        message += f'\nСтартовая цена аукциона: {req.get("auction_start_price")}'
+        if req.get('max_bid_amount') is None:
+            message += f'\nСтартовая цена аукциона: {bid}'
 
     if req.get('max_bid_amount') is not None:
-        button_bid = InlineKeyboardButton(text=f'Сделать ставку {round(float(req.get("max_bid_amount")*1.05), 2)} {req.get("currency")}',
-                                          callback_data=f"bid_{id}_{round((float(req.get('max_bid_amount'))*1.05), 2)}_{auction_id}")
+        button_bid = InlineKeyboardButton(text=f'Сделать ставку {new_bid} {req.get("currency")}',
+                                          callback_data=f"bid_{id}_{new_bid}_{auction_id}")
         message += f'\nТекущая ставка: {req.get("max_bid_amount")}\nДата окончания аукциона: {req.get("auction_end_date")}'
         keyboard.add(button_bid)
     else:
-        button_bid = InlineKeyboardButton(text=f'Сделать первую ставку {round(float(req.get("auction_start_price")), 2)} {req.get("currency")}',
-                                          callback_data=f"bid_{id}_{round(float(req.get('auction_start_price')), 2)}_{auction_id}")
+        button_bid = InlineKeyboardButton(text=f'Сделать первую ставку {new_bid} {req.get("currency")}',
+                                          callback_data=f"bid_{id}_{new_bid}_{auction_id}")
         keyboard.add(button_bid)
-
-    button_buy = InlineKeyboardButton(text='Купить', callback_data=f"buy_{id}")
-    keyboard.add(button_buy)
 
     if len(media) > 0:
         await bot.send_message(chat_id, text='Загружаю фото... Подождите')
@@ -124,6 +132,7 @@ async def get_menu(chat_id):
 
 async def post_bid(lot_id, auction_id, amount, chat_id, user_id):
     req = bot_api.post_bid(lot_id=lot_id,auction_id=auction_id, amount=amount, user_id=user_id)
+
 
 @bot.message_handler(commands=['start'])
 async def bot_home(message):
@@ -207,7 +216,6 @@ async def handle_callback_query(call):
         await bot.send_message(call.message.chat.id, text=message)
 
     if call.data.startswith('cancel_buy_'):
-        print(call)
         await bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
 
         id = call.data.split('_')[2]
@@ -216,19 +224,48 @@ async def handle_callback_query(call):
         await bot.send_message(call.message.chat.id, text=message)
         await get_lots(call.message.chat.id)
 
-    if call.data.startswith('counteroffer_'):
-        await bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
-        id = call.data.split('_')[1]
-        user_id = call.from_user.id
-        user_nickname = call.from_user.username
-
-        await bot.send_message(call.message.chat.id, text='Введите желаемую цену')
+    # if call.data.startswith('counteroffer_'):
+    #     await bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
+    #     id = call.data.split('_')[1]
+    #     user_id = call.from_user.id
+    #     user_nickname = call.from_user.username
+    #
+    #     await bot.send_message(call.message.chat.id, text='Введите желаемую цену')
 
     if call.data.startswith('bid_'):
+        await bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
         lot_id = call.data.split('_')[1]
         auction_id = call.data.split('_')[3]
         amount = call.data.split('_')[2]
+        lot = bot_api.get_lot(lot_id)
+
+        question_text = f"Хотите поучаствовать в аукционе за лот \"{lot.get('name')}\"?"
+        keyboard = InlineKeyboardMarkup()
+        button_yes = InlineKeyboardButton(text='Да', callback_data=f"confirm_bid_{lot_id}_{auction_id}_{amount}")
+        button_no = InlineKeyboardButton(text='Нет', callback_data=f"cancel_bid_{lot_id}_{auction_id}_{amount}")
+        keyboard.add(button_yes, button_no)
+        await bot.send_message(call.message.chat.id, text=question_text, reply_markup=keyboard)
+
+    if call.data.startswith('confirm_bid_'):
+        await bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
+        lot_id = call.data.split('_')[2]
+        auction_id = call.data.split('_')[3]
+        amount = call.data.split('_')[4]
+        lot = bot_api.get_lot(lot_id)
         await post_bid(lot_id, auction_id, amount, call.message.chat.id, call.from_user.username)
 
+        message = f"""Спасибо!
+
+Ваша ставка {amount} {lot.get('currency')} за \"{lot.get('name')}\" принята.
+По всем вопросам пишите @slvjzz"""
+        await bot.send_message(call.message.chat.id, text=message)
+
+    if call.data.startswith('cancel_bid_'):
+        await bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
+        id = call.data.split('_')[2]
+        lot = bot_api.get_lot(id)
+        message = f"Жаль, что передумали покупать {lot.get('name')}. Возвращаюсь к списку лотов."
+        await bot.send_message(call.message.chat.id, text=message)
+        await get_lots(call.message.chat.id)
 
 asyncio.run(bot.polling(none_stop=True, interval=0))
